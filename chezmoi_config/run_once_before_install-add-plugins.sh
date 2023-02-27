14d127a43f9b6eff255d4ae04d6aff1681b6321c
nolte <nolte07@gmail.com>
2023-02-28 00:07:35 +0100

    only add the plugin if not exists

--- chezmoi_config/run_once_before_install-add-plugins.sh
+++ chezmoi_config/run_once_before_install-add-plugins.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 
-asdf plugin-add vault
-asdf plugin-add terragrunt
+set -e
+set -u
+set -o pipefail
+
+asdfAddPluginIfNotExists() {
+    PLUGIN_NAME=$1
+    if asdf plugin list | grep -q $PLUGIN_NAME; then
+        echo "$PLUGIN_NAME plugin already installed"
+    else
+        asdf plugin-add $PLUGIN_NAME
+    fi
+}
+
+asdfAddPluginIfNotExists vault
+asdfAddPluginIfNotExists terragrunt
