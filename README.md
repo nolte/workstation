dc587c71d91777bc7c5c1c663cf6cce81303994a
nolte <nolte07@gmail.com>
2023-02-28 17:07:58 +0100

    add talosctl

--- README.md
+++ README.md
@@ -13,6 +13,12 @@ This Repository will be used for Configure a Different set of Workstations.
 
 ## Configurations
 
+Usefull Commands [chezmoi.io](https://www.chezmoi.io/quick-start/#start-using-chezmoi-on-your-current-machine)
+
+```sh
+
+```
+
 ### ASDF
 
 Manage a set of extra Repositoryies, not managed at [asdf-vm/asdf-plugins](https://github.com/asdf-vm/asdf-plugins/tree/master/plugins)
--- chezmoi_config/run_once_before_install-add-plugins.sh
+++ chezmoi_config/run_once_before_install-add-plugins.sh
@@ -6,12 +6,14 @@ set -o pipefail
 
 asdfAddPluginIfNotExists() {
     PLUGIN_NAME=$1
+    PLUGIN_REPO=$2
     if asdf plugin list | grep -q $PLUGIN_NAME; then
         echo "$PLUGIN_NAME plugin already installed"
     else
-        asdf plugin-add $PLUGIN_NAME
+        asdf plugin-add $PLUGIN_NAME $PLUGIN_REPO
     fi
 }
 
 asdfAddPluginIfNotExists vault
 asdfAddPluginIfNotExists terragrunt
+asdfAddPluginIfNotExists talosctl https://github.com/nolte/asdf-talosctl.git
